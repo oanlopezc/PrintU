421c00b859804a50bc69cf7c47d6615dd71bbc4b
CamiloAMolina <48130741+CamiloAMolina@users.noreply.github.com>
2019-07-24 22:30:54 -0500

    Create config_pdf.php

---
+++ App/Upload_doc/config_pdf.php
@@ -0,0 +1,13 @@
+<?php
+ // config.php
+ // Credenciales
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpwd = "raspberry";
+ //$dbpass = "";
+ $dbname = "printU";
+ //Conexion con la base de datos
+ $con = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
+?>
+
+
